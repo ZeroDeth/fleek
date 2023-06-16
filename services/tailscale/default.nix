e8273cb05290fc75d85318510c9da951082a1147
Sherif Abdalla <sherif@abdalla.uk>
2023-06-16 21:37:25 +0100

    fleek apply

--- services/tailscale/default.nix
+++ services/tailscale/default.nix
@@ -1,27 +1,26 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 with lib;
 
 let
   cfg = config.services.tailscale;
 
-in
-{
-  config = mkIf cfg.enable {
-    environment.systemPackages = [ cfg.package ];
-    launchd.daemons.tailscale = {
-      serviceConfig = {
-        Label = "com.tailscale.tailscale";
-        ProgramArguments = [
-          "/bin/sh"
-          "-c"
-          "/bin/wait4path ${cfg.package} &amp;&amp; sleep 10 &amp;&amp; sudo ${cfg.package}/bin/tailscale up"
-        ];
-        RunAtLoad = true;
-        KeepAlive = {
-          SuccessfulExit = false;
+in {
+    config = mkIf cfg.enable {
+      environment.systemPackages = [ cfg.package ];
+      launchd.daemons.tailscale = {
+        serviceConfig = {
+          Label = "com.tailscale.tailscale";
+          ProgramArguments = [
+            "/bin/sh"
+            "-c"
+            "/bin/wait4path ${cfg.package} &amp;&amp; sleep 10 &amp;&amp; sudo ${cfg.package}/bin/tailscale up"
+          ];
+          RunAtLoad = true;
+          KeepAlive = {
+            SuccessfulExit = false;
+          };
         };
       };
     };
-  };
-}
+  }
